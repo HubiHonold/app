2d4c8d46ca5e879a2108189e16bc59cf61f372ef
primozratej <primozratej@gmail.com>
2024-02-25 17:11:28 +0700

    Flavored app flavor config

--- lib/main.dart
+++ lib/main.dart
@@ -2,33 +2,19 @@ import 'package:flavor_getter/flavor_getter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import 'package:humhub/util/intent/intent_plugin.dart';
+import 'package:humhub/opener_app.dart';
 import 'package:humhub/util/log.dart';
-import 'package:humhub/util/notifications/plugin.dart';
-import 'package:humhub/util/push/push_plugin.dart';
-import 'package:humhub/util/router.dart';
 import 'package:loggy/loggy.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 main() async {
-  // Retrieve flavor argument
-  //String? flavor = appFlavor;
-  String flavor = 'Unknown';
-  final flavorGetterPlugin = FlavorGetter();
-  try {
-    flavor = await flavorGetterPlugin.getFlavor() ?? 'Unknown flavor';
-  } on PlatformException {
-    flavor = 'Failed to get flavor';
-  }
-
   Loggy.initLoggy(
     logPrinter: const GlobalLog(),
   );
   WidgetsFlutterBinding.ensureInitialized();
-  await clearSecureStorageOnReinstall();
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then((_) {
-    runApp(const ProviderScope(child: MyApp()));
+    FlavorGetter().getFlavor().then((value) {
+      runApp(const ProviderScope(child: MyApp()));
+    });
   });
 }
 
@@ -42,40 +28,15 @@ class MyApp extends ConsumerStatefulWidget {
 class MyAppState extends ConsumerState<MyApp> {
   @override
   Widget build(BuildContext context) {
-    clearSecureStorageOnReinstall();
-    return IntentPlugin(
-      child: NotificationPlugin(
-        child: PushPlugin(
-          child: FutureBuilder<String>(
-            future: MyRouter.getInitialRoute(ref),
-            builder: (context, snap) {
-              if (snap.hasData) {
-                return MaterialApp(
-                  debugShowCheckedModeBanner: false,
-                  initialRoute: snap.data,
-                  routes: MyRouter.routes,
-                  navigatorKey: navigatorKey,
-                  theme: ThemeData(
-                    fontFamily: 'OpenSans',
-                  ),
-                );
-              }
-              return const SizedBox.shrink();
-            },
-          ),
-        ),
-      ),
+    return FutureBuilder<String?>(
+      future: FlavorGetter().getFlavor(),
+      builder: (context, snap) {
+        logDebug('Flavor: ${snap.data}');
+        if (snap.connectionState == ConnectionState.done) {
+          return snap.hasData && snap.data != null ? Container() : const OpenerApp();
+        }
+        return const SizedBox.shrink();
+      },
     );
-  }
-}
-
-clearSecureStorageOnReinstall() async {
-  String key = 'hasRunBefore';
-  SharedPreferences prefs = await SharedPreferences.getInstance();
-  bool hasRunBefore = prefs.getBool(key) ?? false;
-  if(!hasRunBefore) {
-    FlutterSecureStorage storage = const FlutterSecureStorage();
-    await storage.deleteAll();
-    prefs.setBool(key, true);
   }
 }
